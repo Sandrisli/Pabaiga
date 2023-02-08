b2a232f19c1cd3eda0afceee60322f5cf8e3f977
Sandrisli <114820899+Sandrisli@users.noreply.github.com>
2023-02-08 20:40:45 +0200

    initial comit

--- client/src/components/Button/Button.jsx
+++ client/src/components/Button/Button.jsx
@@ -1,10 +1,10 @@
 import styled from "syled-components";
 
 const ButtonStyled = styled.button`
-    background-color: f37bc6; 
-    border: 1px solid purple;
+    background-color: ae6191; 
+    border: 1px solid E04F80;
     border-radius: 12px;
-    color:  16010e; 
+    color:  f592cf; 
     font-size: 16px;
     padding: 10px 20px;
 
